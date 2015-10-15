40b0810278988a32b7c4d8a264b46a6c7ac42a0c
Javier Roldán <javirolga@gmail.com>
2015-10-16 01:42:59 +0200

    proyecto rdy

---
+++ src/Arbol.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Yo-PC
+ */
+public class Arbol extends SearchAlgorithm{
+
+    
+    @Override
+    public void searchPath() {
+        System.out.println("hola");
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+        ArrayList<Action> abiertos = new ArrayList<Action>();
+
+        while(!abiertos.isEmpty()){
+            
+        }
+        
+    }
+    
+
+    @Override
+    public ArrayList<Action> result() {
+        System.out.println("hola");
+        ArrayList<Action> resultado = new ArrayList<Action>();
+        return resultado;
+        //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
