f0c068569c1e095222937db3dd65db9e73a9b6a8
Javier Roldán <javirolga@gmail.com>
2015-10-16 03:40:19 +0200

    ¿return?

--- src/Arbol.java
+++ src/Arbol.java
@@ -11,18 +11,66 @@ import java.util.ArrayList;
  *
  * @author Yo-PC
  */
-public class Arbol extends SearchAlgorithm{
+public class Arbol extends SearchAlgorithm /*implements SearchProblem*/{
 
+    /*
+    @Override
+    public State initialState() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //State estadoInicial = new State(0,0);
+        return problem.initialState();
+    }
+
+    @Override
+    public State goalState() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return problem.goalState();
+        
+    }
+
+    @Override
+    public State applyAction(State state, Action action) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Action> getActions(State state) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double cost(State state, Action action) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean checkPath(ArrayList<Action> solution) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    */
     
     @Override
     public void searchPath() {
-        System.out.println("hola");
+        int i = 0;
+        //System.out.println("hola");
+        
+        Node inicio = new Node(problem.initialState());
+        Node expandir;
+        //State estado = new State(2,4);
+        //System.out.println(estado.toString());
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    
-        ArrayList<Action> abiertos = new ArrayList<Action>();
-
+ 
+        //ArrayList<Action> abiertos = new ArrayList<Action>();
+        ArrayList<Node> abiertos = new ArrayList<Node>();
+        abiertos.add(inicio);
+        //System.out.println("purazo: " + abiertos.get(0).getState().toString());
+        
+        //abierto.add();
         while(!abiertos.isEmpty()){
             
+            expandir = abiertos.get(0);
+            if(expandir.equals(problem.goalState())) return expandir;
         }
         
     }
@@ -30,10 +78,12 @@ public class Arbol extends SearchAlgorithm{
 
     @Override
     public ArrayList<Action> result() {
-        System.out.println("hola");
+        System.out.println("hola2");
         ArrayList<Action> resultado = new ArrayList<Action>();
         return resultado;
         //To change body of generated methods, choose Tools | Templates.
     }
+
+    
     
 }
